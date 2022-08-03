d43f75af0fab0400447b53f0c582a3d5edaf4ae6
Aitor Lancharro <aitor.lancharro@gmail.com>
2022-08-03 20:52:29 +0200

    burger menu finished

--- hamburgerMenu.js
+++ hamburgerMenu.js
@@ -7,10 +7,12 @@ const menuIcon = document.querySelector(".menuIcon");
 function toggleMenu() {
   if (menu.classList.contains("showMenu")) {
     menu.classList.remove("showMenu");
+    hamburger.classList.remove("closeGround");
     closeIcon.style.display = "none";
     menuIcon.style.display = "block";
   } else {
     menu.classList.add("showMenu");
+    hamburger.classList.add("closeGround");
     closeIcon.style.display = "block";
     menuIcon.style.display = "none";
   }
